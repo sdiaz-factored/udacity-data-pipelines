b1696a5b7cec9ed2ea7be36cf8c1000701623793
sdiaz-factored <sebastian.diaz@factored.ai>
2023-06-25 15:30:00 -0500

    dag added

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -2,38 +2,46 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
-class DataQualityOperator(BaseOperator):
 
-    ui_color = '#89DA59'
+class DataQualityOperator(BaseOperator):
+    ui_color = "#89DA59"
 
     @apply_defaults
-    def __init__(self,
-                 postgres_conn_id = '',
-                 tests = [], # List of sql tests
-                 *args, **kwargs):
-
+    def __init__(
+        self,
+        postgres_conn_id="",
+        tests=[],  # List of sql tests
+        *args,
+        **kwargs,
+    ):
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         self.postgres_conn_idd = postgres_conn_id
         self.tests = tests
 
     def execute(self, context):
         postgres = PostgresHook(self.postgres_conn_idd)
-        self.log.info('Postres connection created for the redshift dw')
-
+        self.log.info("Postres connection created for the redshift dw")
+        self.log.info(f"Starting validation of tests")
         for test in self.tests:
-            expected = test['expected']
-            type = test.get('type', 'general')
-            sql = test['sql']
+            self.log.info(f"Running test {test}")
+            expected = test["expected"]
+            type = test.get("type", "general")
+            sql = test["sql"]
             self.log.info(f'Get records for query: "{sql}"')
             records = postgres.get_records(sql)
             num_records = records[0][0]
 
-            if type == 'count' and num_records < expected:
-                raise ValueError(f"Data quality check failed.\
+            if type == "count" and num_records < expected:
+                raise ValueError(
+                    f"Data quality check failed.\
                     The sql query {test.sql_query} returned\
-                        the following results {records}")
-                
+                        the following results {records}"
+                )
+
             elif num_records >= expected:
-                raise ValueError(f"Data quality check failed.\
+                raise ValueError(
+                    f"Data quality check failed.\
                      The sql query {test.sql_query} returned\
-                          the following results {records}")
+                          the following results {records}"
+                )
+        self.log.info(f"Successful validation")
